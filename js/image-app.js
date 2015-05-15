da141bf5816c0f835ac273ff5ee68da48cb9a4b5
rafaelogando <rafael-ogando@hotmail.com>
2015-05-15 12:04:13 -0400

    new info

---
+++ js/image-app.js
@@ -0,0 +1,73 @@
+(function(){
+  // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
+  var original;
+  var imageLoader = document.querySelector('#imageLoader');
+  imageLoader.addEventListener('change', handleImage, false);
+  var canvas = document.querySelector('#image');
+  var ctx = canvas.getContext('2d');
+
+  function handleImage(e){
+    var reader = new FileReader();
+    reader.onload = function(event){
+      var img = new Image();
+      img.onload = function(){
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img,0,0);
+        original = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      }
+      img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
+
+  // greys out the buttons while manipulation is happening
+  // un-greys out the buttons when the manipulation is done
+  function toggleButtonsAbledness() {
+    var buttons = document.querySelectorAll('button');
+    for (var i = 0; i < buttons.length; i++) {
+      if (buttons[i].hasAttribute('disabled')) {
+        buttons[i].removeAttribute('disabled')
+      } else {
+        buttons[i].setAttribute('disabled', null);
+      }
+    };
+  }
+
+  function manipulateImage(type) {
+    var a, b, g, i, imageData, j, length, pixel, r, ref;
+    imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    toggleButtonsAbledness();
+
+    var myWorker = new Worker("worker.js");
+    myWorker.postMessage(type,imageData);
+
+    myWorker.onmessage=function(e){imageData=e.data; type=imageData.type;};
+    // Hint! This is where you should post messages to the web worker and
+    // receive messages from the web worker.
+
+    toggleButtonsAbledness();
+    return ctx.putImageData(imageData, 0, 0);
+  };
+
+  function revertImage() {
+    return ctx.putImageData(original, 0, 0);
+  }
+
+  document.querySelector('#invert').onclick = function() {
+    manipulateImage("invert");
+  };
+  document.querySelector('#chroma').onclick = function() {
+    manipulateImage("chroma");
+  };
+  document.querySelector('#greyscale').onclick = function() {
+    manipulateImage("greyscale");
+  };
+  document.querySelector('#vibrant').onclick = function() {
+    manipulateImage("vibrant");
+  };
+  document.querySelector('#revert').onclick = function() {
+    revertImage();
+  };
+})();
